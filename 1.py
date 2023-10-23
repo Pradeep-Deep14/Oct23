faba77d3768b8c95dae7abd26177f09afffc099b
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-10-23 09:25:02 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,5 @@
+my_list=[2,4,6,8,10]
+result=[x*2 for x in my_list if x%3==0]
+print(result)
+
+#[12]
---
+++ 4.py
@@ -0,0 +1,9 @@
+def func(x):
+    try:
+        print(20/x)
+    except:
+        print('Invalid argument',end=" ")
+print(func(0))
+
+
+#Invalid Argument None
