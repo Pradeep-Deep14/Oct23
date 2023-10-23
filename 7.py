bebeb2cf6db7a33e26748b383f749216f56e08be
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-10-23 16:57:22 +0530

    first commit

---
+++ 7.py
@@ -0,0 +1,6 @@
+def my_function(**kwargs):
+    results = sum(kwargs.values())
+    return results
+print(my_function(a = 23, b = 12, c= 67, d = 100)) # Keyword arguments
+
+#oru dictionary mari seyal pada aramichidum
